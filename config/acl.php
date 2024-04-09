b7f695d3dc51c8bbda53a29d913fe1b2ea8c0e9e
Anton Raharja <araharja@pm.me>
2024-04-09 14:11:31 +0000

    update method names

--- config/acl.php
+++ config/acl.php
@@ -3,10 +3,11 @@
 return [
 
 	/**
-	 * List of all models
+	 * List of all enabled models
 	 */
 	'models' => [
 		'api',
+		'general',
 		'role',
 		'user',
 		'permission',
@@ -24,19 +25,19 @@ return [
 
 	/**
 	 * Default method names
+	 * Ref: https://filamentphp.com/docs/3.x/panels/resources/getting-started#authorization
 	 */
 	'methods' => [
-		'index',
-		'viewany',
-		'view',
-		'create',
-		'update',
-		'delete',
-		'restore',
-		'forcedelete',
-		'store',
-		'edit',
-		'destroy',
-		'list',
+		'create',			// create record
+		'delete',			// delete record
+		'deleteany',		// bulk delete records
+		'forcedelete',		// force delete records
+		'forcedeleteany',	// force bulk delete records
+		'reorder',			// reorder record
+		'restore',			// restore record
+		'restoreany',		// bulk restore records
+		'update',			// update record
+		'view',				// view record
+		'viewany',			// entry access to records
 	],
 ];
