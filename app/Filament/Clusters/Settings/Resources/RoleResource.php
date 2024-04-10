4a5ee18174658099f8a4009b25d3e6b3452a37d0
Anton Raharja <araharja@pm.me>
2024-04-10 10:57:58 +0000

    add filter and small fixes on Role

--- app/Filament/Clusters/Settings/Resources/RoleResource.php
+++ app/Filament/Clusters/Settings/Resources/RoleResource.php
@@ -3,16 +3,12 @@
 namespace App\Filament\Clusters\Settings\Resources;
 
 use Filament\Forms;
-use Filament\Tables;
 use Filament\Forms\Form;
 use Base\ACL\Models\Role;
-use Filament\Tables\Table;
 use Filament\Resources\Resource;
 use App\Filament\Clusters\Settings;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
-use Filament\Tables\Columns\TagsColumn;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
 use App\Filament\Clusters\Settings\Resources\RoleResource\Pages;
 
@@ -39,7 +35,7 @@ class RoleResource extends Resource
 							->minLength(3)
 							->maxLength(30)
 							->lazy()
-							->afterStateUpdated(fn($state, Forms\Set $set) => $set('name', str_replace(' ', '_', strtoupper((string) $state))))
+							->afterStateUpdated(fn($state, Forms\Set $set) => $set('name', preg_replace('/\s+/', '_', strtoupper((string) $state))))
 							->hint(__('Max. 30 chars')),
 						TextInput::make('description'),
 						Select::make('permissions.name')
@@ -50,53 +46,6 @@ class RoleResource extends Resource
 							->preload()
 							->native(false),
 					])
-			]);
-	}
-
-	public static function table(Table $table): Table
-	{
-		return $table
-			->reorderable('order_column')
-			->defaultSort('order_column')
-			->columns([
-				TextColumn::make('name')
-					->sortable()
-					->searchable(),
-				TextColumn::make('description')
-					->sortable()
-					->searchable()
-					->toggleable(isToggledHiddenByDefault: true),
-				TagsColumn::make('permissions.name')
-					->sortable()
-					->searchable(),
-				TextColumn::make('created_at')
-					->label(__('Created'))
-					->dateTime()
-					->timezone(\Base\General\Facades\General::getTimezone())
-					->sortable()
-					->toggleable(isToggledHiddenByDefault: true),
-				TextColumn::make('updated_at')
-					->label(__('Updated'))
-					->dateTime()
-					->timezone(\Base\General\Facades\General::getTimezone())
-					->sortable()
-					->toggleable(isToggledHiddenByDefault: true),
-			])
-			->filters([
-				//
-			])
-			->actions([
-				Tables\Actions\DeleteAction::make()
-					->label('')
-					->tooltip(_('Delete')),
-				Tables\Actions\EditAction::make()
-					->label('')
-					->tooltip(__('Edit')),
-			])
-			->bulkActions([
-				Tables\Actions\BulkActionGroup::make([
-					Tables\Actions\DeleteBulkAction::make(),
-				]),
 			]);
 	}
 
--- app/Filament/Clusters/Settings/Resources/RoleResource/Pages/ListRoles.php
+++ app/Filament/Clusters/Settings/Resources/RoleResource/Pages/ListRoles.php
@@ -2,18 +2,84 @@
 
 namespace App\Filament\Clusters\Settings\Resources\RoleResource\Pages;
 
+use Filament\Tables;
 use Filament\Actions;
+use Base\ACL\Facades\ACL;
+use Filament\Tables\Table;
+use Base\General\Facades\General;
+use Filament\Tables\Columns\TagsColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Filters\SelectFilter;
 use App\Filament\Clusters\Settings\Resources\RoleResource;
 
 class ListRoles extends ListRecords
 {
-    protected static string $resource = RoleResource::class;
+	protected static string $resource = RoleResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
+	protected function getHeaderActions(): array
+	{
+		return [
+			Actions\CreateAction::make(),
+		];
+	}
+
+	public function table(Table $table): Table
+	{
+		return $table
+			->reorderable('order_column')
+			->defaultSort('order_column')
+			->columns([
+				TextColumn::make('name')
+					->sortable()
+					->searchable(),
+				TextColumn::make('description')
+					->sortable()
+					->searchable()
+					->toggleable(isToggledHiddenByDefault: true),
+				TagsColumn::make('permissions.name')
+					->sortable()
+					->searchable(),
+				TextColumn::make('created_at')
+					->label(__('Created'))
+					->dateTime()
+					->timezone(General::getTimezone())
+					->sortable()
+					->toggleable(isToggledHiddenByDefault: true),
+				TextColumn::make('updated_at')
+					->label(__('Updated'))
+					->dateTime()
+					->timezone(General::getTimezone())
+					->sortable()
+					->toggleable(isToggledHiddenByDefault: true),
+			])
+			->filters([
+				SelectFilter::make('name')
+					->label(__('Roles type'))
+					->placeholder(__('Select options'))
+					->multiple()
+					->options(function () {
+						$roles = [];
+						foreach ( ACL::config()->allRoles as $role ) {
+							$roles[$role] = $role;
+						}
+
+						return $roles;
+					})
+					->native(false),
+			])
+			->actions([
+				Tables\Actions\DeleteAction::make()
+					->label('')
+					->tooltip(_('Delete')),
+				Tables\Actions\EditAction::make()
+					->label('')
+					->tooltip(__('Edit')),
+			])
+			->bulkActions([
+				Tables\Actions\BulkActionGroup::make([
+					Tables\Actions\DeleteBulkAction::make(),
+				]),
+			]);
+	}
 }
