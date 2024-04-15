487321af7267bfa65d486f1285a087624470ec33
Anton Raharja <araharja@pm.me>
2024-04-15 04:06:34 +0000

    change remoteAddr to remotes

--- app/Base/ACL/Checkers/IP.php
+++ app/Base/ACL/Checkers/IP.php
@@ -36,9 +36,9 @@ class IP implements CheckerInterface
 		}
 
 		foreach ( $items as $item ) {
-			$remoteAddr = config('acl.remoteAddr');
-			if (is_array($remoteAddr)) {
-				foreach ( $remoteAddr as $addr ) {
+			$remotes = config('acl.remotes');
+			if (is_array($remotes)) {
+				foreach ( $remotes as $addr ) {
 					if (isset($_SERVER[$addr])) {
 						if (matchIP($item, $_SERVER[$addr])) {
 							$this->permittedEntry = $item;
