dcb4fc444fa7f3a63a7b06e1def4b572b661e5f9
Anton Raharja <araharja@pm.me>
2024-04-16 03:30:39 +0000

    register blade if for ACL have dontHave role and isAdmin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,38 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		Blade::if('have', function (string|array $permissions) {
+			return auth()->user()->have($permissions);
+		});
+
+		Blade::if('dontHave', function (string|array $permissions) {
+			return auth()->user()->dontHave($permissions);
+		});
+
+		Blade::if('role', function (string|array $role) {
+			return auth()->user()->role($role);
+		});
+
+		Blade::if('isAdmin', function () {
+			return auth()->user()->role(aclhc('ADMIN'));
+		});
+	}
 }
