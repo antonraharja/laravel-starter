3602094f1d967bfaa6ae4c9cae1fbe9d486dba3c
Anton Raharja <araharja@pm.me>
2024-05-07 03:23:13 +0000

    fix role user model

--- app/Base/ACL/Models/Role.php
+++ app/Base/ACL/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace Base\ACL\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
